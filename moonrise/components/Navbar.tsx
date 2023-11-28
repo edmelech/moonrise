800fb06a6064715ba34498ac76c7988227585d97
Ed Melech <mr.melech@gmail.com>
2023-11-28 18:53:18 +0000

    fixed flex of Navbar

--- moonrise/components/Navbar.tsx
+++ moonrise/components/Navbar.tsx
@@ -4,18 +4,18 @@ import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <nav className="bg-white flexBetween max-container padding-container relative z-30 py-5">
+    <nav className="bg-white flex justify-between max-container padding-container relative z-30 p-5">
       <Link href="/">
         <Image src="/moonrise-logo.svg" alt="logo" width={200} height={100} />
-
-        <ul className='hidden h-full gap-12 lg:flex text-red-500'>
-          {NAV_LINKS.map((link) => (
-            <Link href={link.href} key={link.key}>
-              {link.label}
-            </Link>
-          ))}
-        </ul>
       </Link>
+      <ul className='hidden h-full gap-12 lg:flex'>
+        {NAV_LINKS.map((link) => (
+          <Link href={link.href} key={link.key} className="regular-16 text-lime-950 
+          flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+            {link.label}
+          </Link>
+        ))}
+      </ul>
     </nav>
   )
 }
