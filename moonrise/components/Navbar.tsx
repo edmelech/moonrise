cb43a98982150ed7996355693f8804688e075cbe
Ed Melech <mr.melech@gmail.com>
2023-12-04 15:49:50 +0000

    added navbar toggle functionality

--- moonrise/components/Navbar.tsx
+++ moonrise/components/Navbar.tsx
@@ -1,16 +1,29 @@
+'use client'
+
+import { useState } from 'react'
 import { NAV_LINKS } from '../constants'
 import Link from 'next/link'
 import Image from 'next/image'
 
 const Navbar = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <nav className="bg-white flex justify-between padding-container relative z-30 p-5">
       <Link href="/">
         <Image src="/moonrise-logo.svg" alt="logo" width={200} height={100} />
       </Link>
-      <ul className='hidden h-full gap-12 lg:flex'>
+      <ul className={`h-full gap-12 lg:flex ${isMenuOpen ? 'block' : 'hidden'}`}>
         {NAV_LINKS.map((link) => (
-          <Link href={link.href} key={link.key} className="regular-16 text-lime-950 
+          <Link 
+            href={link.href} 
+            key={link.key} 
+            className="regular-16 text-lime-950 
           flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
             {link.label}
           </Link>
@@ -23,6 +36,7 @@ const Navbar = () => {
             width={32}
             height={32}
             className='inline-block cursor-pointer lg:hidden'
+            onClick={toggleMenu}
           />
 
     </nav>
