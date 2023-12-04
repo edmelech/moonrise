8242929effec5c1513ec86584dfa8b70857ac1b8
Ed Melech <mr.melech@gmail.com>
2023-12-04 17:27:37 +0000

    start building out hero section

--- moonrise/app/page.js
+++ moonrise/app/page.js
@@ -1,10 +1,10 @@
+import Hero from '@/components/Hero'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
-      <h1 className='font-bold text-2xl'>Hello World</h1>
-      <button className='btn btn-primary'>Hiyaaaa</button>
+      <Hero />
     </main>
     
   )
---
+++ moonrise/components/Hero.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500'>
+
+    {/* LEFT */}
+
+    <div className="bg-green-800 text-white p-4 h-screen max-w-screen/2 mx-auto">
+      <h1 className="text-3xl font-bold">The high-water mark in civil engineering</h1>
+      <p className="text-lg">A new way to discover and support indie creators</p>
+    </div>
+
+    
+
+    </section>
+  )
+}
+
+export default Hero
