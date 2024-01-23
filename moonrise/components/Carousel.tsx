28615985265f7adb29e49a5103d35719ce43e1e2
Ed Melech <mr.melech@gmail.com>
2024-01-23 19:16:18 +0000

    build out carousel

--- moonrise/components/Carousel.tsx
+++ moonrise/components/Carousel.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const images = [
+  '/images/Candidates.jpg',
+  '/images/public/water-treatment-plant.jpg',
+  '/images/public/Water-Engineer-Clipboard.jpg',
+]
 
 const Carousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
   return (
-    <div>Carousel</div>
+    <Slider {...settings}>
+      {images.map((image, index) => (
+        <div key={index}>
+          <img src={image} alt={`Carousel Image ${index + 1}`} />
+        </div>
+      ))}
+    </Slider>
   )
 }
 
--- moonrise/components/Hero.tsx
+++ moonrise/components/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Carousel from './Carousel';
 
 const Hero = () => {
   return (
@@ -12,8 +13,7 @@ const Hero = () => {
     </div>
 
     <div className="bg-green-800 text-white p-4 h-screen mx-auto border-2 border-red-500">
-      <h1 className="text-3xl font-bold">I will be the carousel</h1>
-      <p className="text-lg">A new way to discover and support indie creators moonrise. is a specialist talent partner in the US civil engineering industry. It is our sole mission to provide first class service to our clients, sourcing ideal candidates from niche markets, creating lasting business relationships. With a vast and expanding network of industry leaders and experts, we have the very best of US engineering talent right at our fingertips. With an emphasis on civil infrastructure, we are recruitment specialists in: Water/Wastewater, Water Treatment, Transportation, Water Resources, and Land Development engineering. We understand how important our clients' work is in creating a sustainable and  </p>
+      <Carousel />
     </div>
 
     </section>
