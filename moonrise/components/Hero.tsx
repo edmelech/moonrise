b207109e942275205bc7d64d7e407673f2e68c32
Ed Melech <mr.melech@gmail.com>
2024-02-06 15:48:40 +0000

    change background colour

--- moonrise/components/Hero.tsx
+++ moonrise/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
 
     {/* LEFT */}
 
-    <div className="bg-green-800 text-white p-16 h-screen w-3/5 min-w-3/5 border-2 border-red-500">
+    <div className="bg-custom-color text-white p-16 h-screen w-3/5 min-w-3/5 border-2 border-red-500">
       <h1 className="text-7xl font-bold pb-8 w-4/5">The high-water mark in civil engineering recruitment</h1>
       <p className="text-lg font-weight: 100">moonrise. is a specialist talent partner in the US civil engineering industry. It is our sole mission to provide first class service to our clients, 
       sourcing ideal candidates from niche markets, creating lasting business relationships. With a vast and expanding network of industry leaders and experts, we have the very best of US engineering talent right at our fingertips. With an emphasis on civil infrastructure, we are recruitment specialists in: Water/Wastewater, Water Treatment, Transportation, Water Resources, and Land Development engineering.</p> 
