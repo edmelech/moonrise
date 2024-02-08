7d4eaf7e0ac31309eedb76419fdcbff9caad786f
Ed Melech <mr.melech@gmail.com>
2024-02-08 18:49:40 +0000

    fix contact page

--- moonrise/constants/index.ts
+++ moonrise/constants/index.ts
@@ -2,7 +2,7 @@ export const NAV_LINKS = [
   { href: '/', key: 'expertise', label: 'Expertise' },
   { href: '/', key: 'locations', label: 'Locations' },
   { href: '/', key: 'vacancies', label: 'Vacancies ' },
-  { href: '/', key: 'contact', label: 'Contact' },
+  { href: '/contact', key: 'contact', label: 'Contact' },
 ];
 
 
