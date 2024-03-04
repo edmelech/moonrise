0ee1d68c8bf10946da213f586e1035954df2a81a
Ed Melech <mr.melech@gmail.com>
2024-03-04 18:01:03 +0000

    continue building out contact form

--- moonrise/components/ContactForm.tsx
+++ moonrise/components/ContactForm.tsx
@@ -1,26 +1,48 @@
 import React from 'react'
 import MoonriseLogo from './MoonriseLogo'
+import CountryList from './CountryList'
 
 const ContactForm: React.FC = () => {
   return (
-    <div className='flex flex-center items-center w-full min-h-screen px-5 py-5 bg-white'>
-      <div className='py-8'>
-        <p className='text-[#003030]'>contact</p>
+    <div className='flex flex-col flex-center items-center w-full min-h-screen px-5 py-5 bg-white'>
+      <div className='flex flex-col py-8 items-center'>
+        <p className='text-[#003030] pb-8'>contact</p>
         <p className='text-[#003030]'>Thank you for your interest in moonrise. To speak with a representative, please complete the form below. One of our experts will follow up with you shortly.</p>
       </div>
 
-      <div> 
-        <form className='flex'>
-          <div className='flex'>
-            <div>
-              <p className='text-[#003030]'>First name</p>
-              <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+      <div className='flex border w-2/3'> 
+        <form className='flex flex-col w-full'>
+          <div className='flex w-full'>
+            <div className='w-1/2 px-2'>
+              <p className='text-[#003030]'>First Name</p>
+              <input type="text" placeholder="Type here" className="input input-bordered w-full" />
             </div>
-            <div>
-              <p className='text-[#003030]'>Last name</p>
-              <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+            <div className='w-1/2 px-2'>
+              <p className='text-[#003030]'>Last Name</p>
+              <input type="text" placeholder="Type here" className="input input-bordered w-full" />
             </div>
           </div>
+          <div className='flex w-full'>
+            <div className='w-1/2 px-2'>
+              <p className='text-[#003030]'>Company</p>
+              <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+            </div>
+            <div className='w-1/2 px-2'>
+              <p className='text-[#003030]'>Job Title</p>
+              <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+            </div>
+          </div>
+          <div className='flex w-full'>
+            <div className='w-1/2 px-2'>
+              <p className='text-[#003030]'>E-mail</p>
+              <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+            </div>
+            <div className='w-1/2 px-2'>
+              <p className='text-[#003030]'>Phone Number</p>
+              <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+            </div>
+          </div>
+          <CountryList />
         </form>
       </div>
     </div>
