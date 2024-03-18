a883dafc33d616f6b33741600008f1e49561987f
Ed Melech <mr.melech@gmail.com>
2024-03-18 22:03:26 +0000

    changed padding and border radius of input boxes

--- moonrise/components/ContactForm.tsx
+++ moonrise/components/ContactForm.tsx
@@ -33,17 +33,17 @@ const ReactHookForm = () => {
 
   return (
     <div className='flex justify-center items-center h-screen'>
-      <form className='tutorial gap-2' onSubmit={handleSubmit(onSubmit)}>
+      <form className='tutorial gap-2 w-96' onSubmit={handleSubmit(onSubmit)}>
         <div className='flex gap-2'>  
           <input {...register("firstName")} 
             type="text" 
             placeholder="First Name" 
-            className='p-3 rounded-md'
+            className='p-3 rounded-md w-full'
           />
           <input {...register("lastName")} 
             type="text" 
             placeholder="Last Name" 
-            className='p-3 rounded-md'
+            className='p-3 rounded-md w-full'
           />
         </div>
 
@@ -51,12 +51,12 @@ const ReactHookForm = () => {
           <input {...register("company")} 
             type="text" 
             placeholder="Company" 
-            className='p-3 rounded-md'
+            className='p-3 rounded-md w-full'
           />
           <input {...register("jobTitle")} 
             type="text" 
             placeholder="Job Title" 
-            className='p-3 rounded-md'
+            className='p-3 rounded-md w-full'
           />
         </div>
         
@@ -64,12 +64,12 @@ const ReactHookForm = () => {
           <input {...register("email")} 
             type="text" 
             placeholder="Email" 
-            className='p-3 rounded-md'
+            className='p-3 rounded-md w-full'
           />
           <input {...register("phoneNumber")} 
             type="text" 
             placeholder="Phone Number" 
-            className='p-3 rounded-md'
+            className='p-3 rounded-md w-full'
           />
         </div>
     
@@ -88,52 +88,3 @@ const ReactHookForm = () => {
 }
 
 export default ReactHookForm
-
-// const ContactForm: React.FC = () => {
-//   return (
-//     <div className='flex flex-col flex-center items-center w-full min-h-screen px-5 py-5 bg-white'>
-//       <div className='flex flex-col py-8 items-center'>
-//         <p className='text-[#003030] pb-8'>contact</p>
-//         <p className='text-[#003030]'>Thank you for your interest in moonrise. To speak with a representative, please complete the form below. One of our experts will follow up with you shortly.</p>
-//       </div>
-
-//       <div className='flex border w-2/3'> 
-//         <form className='flex flex-col w-full'>
-//           <div className='flex w-full'>
-//             <div className='w-1/2 px-2'>
-//               <p className='text-[#003030]'>First Name</p>
-//               <input type="text" placeholder="Type here" className="input input-bordered w-full" />
-//             </div>
-//             <div className='w-1/2 px-2'>
-//               <p className='text-[#003030]'>Last Name</p>
-//               <input type="text" placeholder="Type here" className="input input-bordered w-full" />
-//             </div>
-//           </div>
-//           <div className='flex w-full'>
-//             <div className='w-1/2 px-2'>
-//               <p className='text-[#003030]'>Company</p>
-//               <input type="text" placeholder="Type here" className="input input-bordered w-full" />
-//             </div>
-//             <div className='w-1/2 px-2'>
-//               <p className='text-[#003030]'>Job Title</p>
-//               <input type="text" placeholder="Type here" className="input input-bordered w-full" />
-//             </div>
-//           </div>
-//           <div className='flex w-full'>
-//             <div className='w-1/2 px-2'>
-//               <p className='text-[#003030]'>E-mail</p>
-//               <input type="text" placeholder="Type here" className="input input-bordered w-full" />
-//             </div>
-//             <div className='w-1/2 px-2'>
-//               <p className='text-[#003030]'>Phone Number</p>
-//               <input type="text" placeholder="Type here" className="input input-bordered w-full" />
-//             </div>
-//           </div>
-//           <CountryList />
-//         </form>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default ContactForm
