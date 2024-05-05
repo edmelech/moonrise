dbb2a340fbc8b509c5f95099f3b52b7ff3539965
Ed Melech <mr.melech@gmail.com>
2024-05-05 15:00:39 +0100

    made hero responsive

--- moonrise/components/Hero.tsx
+++ moonrise/components/Hero.tsx
@@ -4,24 +4,24 @@ import MoonriseLogo from './MoonriseLogo';
 
 const Hero = () => {
   return (
-    <section className='flex'>
+    <section className='flex flex-col-reverse md:flex-row'>
 
     {/* LEFT */}
 
-    <div className="bg-custom-color text-white p-16 h-screen w-3/5 min-w-3/5 ">
-      <h1 className="text-7xl font-bold pb-12 w-4/5">The high-water mark in civil engineering recruitment</h1>
-      <p className="text-lg font-weight: 100">moonrise. is a specialist talent partner in the North American infrastructure industry. It is our sole mission to provide first class service to our clients, sourcing ideal candidates from niche markets and creating lasting business relationships. Boasting a deep and expanding network of industry leaders and experts across the US and Canada, we have the very best talent right at our fingertips. With an emphasis on civil infrastructure, we are recruitment specialists in: Engineering, Architecture, Real Estate, Construction, Automation & Controls, and Renewable Energy.</p> 
+    <div className="bg-custom-color text-white p-8 md:p-16 min-h-screen md:w-3/5 md:min-w-3/5 ">
+      <h1 className="text-4xl md:text-7xl font-bold pb-4 md:pb-12">The high-water mark in civil engineering recruitment</h1>
+      <p className="text-base md:text-lg font-light">moonrise. is a specialist talent partner in the North American infrastructure industry. It is our sole mission to provide first class service to our clients, sourcing ideal candidates from niche markets and creating lasting business relationships. Boasting a deep and expanding network of industry leaders and experts across the US and Canada, we have the very best talent right at our fingertips. With an emphasis on civil infrastructure, we are recruitment specialists in: Engineering, Architecture, Real Estate, Construction, Automation & Controls, and Renewable Energy.</p> 
       <br />
-      <p className="text-lg font-weight: 100">We understand how important our clients' work is in creating a sustainable and functioning environment that works for everybody. Our clients trust us because we believe in what they do, whilst we spend all of our time developing our knowledge of the market in real time with genuine passion to ultimately deliver the very best talent solutions.</p>
+      <p className="text-base md:text-lg font-light">We understand how important our clients' work is in creating a sustainable and functioning environment that works for everybody. Our clients trust us because we believe in what they do, whilst we spend all of our time developing our knowledge of the market in real time with genuine passion to ultimately deliver the very best talent solutions.</p>
       <br />
-      <p className='text-lg font-weight: 100 text-center'>We are passionate. We are unique. We are experts. We are...</p>
-      <div className='hero-logo flex items-center justify-center w-1/4 mx-auto p-4 pt-2 '>
+      <p className='text-base md:text-lg font-light text-center pb-4 md:pb-0'>We are passionate. We are unique. We are experts. We are...</p>
+      <div className='hero-logo flex items-center justify-center w-1/2 md:w-1/4 mx-auto p-4 pt-2'>
         <MoonriseLogo className="w-full h-auto"/>
       </div>
-      <div className='text-lg font-weight:100 text-center'>The high-water mark in recruitment.</div>
+      <div className='text-base md:text-lg font-light text-center'>The high-water mark in recruitment.</div>
     </div>
     
-    <div className="h-screen w-2/5 overflow-hidden">
+    <div className="md:h-screen md:w-2/5 overflow-hidden">
       <img 
         src="resize-location-mid.webp" 
         alt="Hero Image" 
