33f136bca46c717ce546e10b0096eb350d1e0d95
Ed Melech <mr.melech@gmail.com>
2024-05-07 16:07:43 +0100

    made column titles bold

--- moonrise/components/Footer.tsx
+++ moonrise/components/Footer.tsx
@@ -25,34 +25,37 @@ const Footer = () => {
             
           </div>
           
-          <div className='flex gap-10 justify-end md:flex-1 text-lime-950 pb-8'>
-            {FOOTER_LINKS.map((columns) => (
-                <FooterColumn title={columns.title}>
-                  <ul className='regular-14 flex flex-col gap-4 text-lime-950'>
-                    {columns.links.map((link) => (
-                      <Link href='/' key={link}>
-                        {link}
-                      </Link>
-                    ))}
-                   </ul> 
-                </FooterColumn>
-              ))}
-          </div>
-              
-          <div className="flex flex-col gap-5 text-lime-950">
-            <FooterColumn title={SOCIALS.title}>
-              <ul className='regular-14 flex flex-col gap-4 text-lime-950'>
-                {SOCIALS.links.map((link) => (
-                  <li key={link.name} className='flex items-center'>
-                    <Link href='/'>
-                      <Image src={link.icon} alt={link.name} width={24} height={24} />
-                    </Link>
-                    <span className='ml-2'>{link.name}</span>
-                  </li>
+          <div className='flex md:flex-row gap-10 justify-end md:flex-1'>
+            <div className='flex gap-10 justify-end md:flex-1 text-lime-950 pb-8 pr-8'>
+              {FOOTER_LINKS.map((columns) => (
+                  <FooterColumn title={columns.title}>
+                    <ul className='regular-14 flex flex-col gap-4 text-lime-950'>
+                      {columns.links.map((link) => (
+                        <Link href='/' key={link}>
+                          {link}
+                        </Link>
+                      ))}
+                    </ul> 
+                  </FooterColumn>
                 ))}
-              </ul>
-            </FooterColumn>
+            </div>
+                
+            <div className="flex flex-col gap-5 text-lime-950">
+              <FooterColumn title={SOCIALS.title}>
+                <ul className='regular-14 flex flex-col gap-4 text-lime-950'>
+                  {SOCIALS.links.map((link) => (
+                    <li key={link.name} className='flex items-center'>
+                      <Link href='/'>
+                        <Image src={link.icon} alt={link.name} width={24} height={24} />
+                      </Link>
+                      <span className='ml-2'>{link.name}</span>
+                    </li>
+                  ))}
+                </ul>
+              </FooterColumn>
+            </div>
           </div>
+          
         </div>
         <div className="flex flex-col w-full">
           <div className="border-t-2 border-lime-950 border-opacity-50"></div> 
@@ -83,7 +86,7 @@ type FooterColumnProps = {
 const FooterColumn = ({ title, children }: FooterColumnProps) => {
   return (
     <div className='flex flex-col gap-5'>
-      <h4 className='Bold-18 whitespace-nowrap'>{title}</h4>
+      <h4 className='whitespace-nowrap'style={{ fontWeight: 'bold' }}>{title}</h4>
       {children}
     </div>
   )
