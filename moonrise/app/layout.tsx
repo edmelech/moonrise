8bf068b607d53b9afbaaacca9f4ffdee434dd6d6
Ed Melech <mr.melech@gmail.com>
2024-05-09 14:46:55 +0100

    fixed toast pop up on contact form submission

--- moonrise/app/layout.tsx
+++ moonrise/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import { ChakraProvider } from '@chakra-ui/react';
 import './globals.css'
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
@@ -7,6 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
   return (
+    <ChakraProvider>
       <div>
         <Navbar />
         <main className='relative overflow-hidden'>
@@ -14,5 +16,6 @@ export default function RootLayout({ children }) {
         </main>
         <Footer />
       </div>
+    </ChakraProvider>
   )
 }
--- moonrise/components/Navbar.tsx
+++ moonrise/components/Navbar.tsx
@@ -11,6 +11,7 @@ const Navbar = () => {
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen)
+    console.log("Menu is open", !isMenuOpen)
   }
 
   return (
@@ -19,15 +20,13 @@ const Navbar = () => {
         <Image src="/moonrise-logo.svg" alt="logo" width={200} height={100} />
       </Link>
       <ul className={`h-full gap-12 lg:flex ${isMenuOpen ? 'block' : 'hidden'}`}>
-        {NAV_LINKS.map((link) => (
-          <Link 
-            href={link.href} 
-            key={link.key} 
-            className="regular-16 text-lime-950 
-          flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
-            {link.label}
+      {NAV_LINKS.map((link) => (
+        <li key={link.key} className="regular-16 text-lime-950 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+          <Link href={link.href}>
+              {link.label}
           </Link>
-        ))}
+        </li>
+      ))}
       </ul>
 
           <Image 
