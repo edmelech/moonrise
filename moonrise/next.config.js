fc8667f73b7cb3426a2224ec615432d0e30a66c9
Ed Melech <mr.melech@gmail.com>
2024-05-15 23:03:19 +0100

    reverted next.congig.js file

--- moonrise/next.config.js
+++ moonrise/next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
-
-    return config
-  }
-}
+module.exports = nextConfig
