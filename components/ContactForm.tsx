cec59352f4cbe009d744f7349ee67cd30e707516
Ed Melech <mr.melech@gmail.com>
2024-05-21 17:18:53 +0100

    added message box and removed other input boxes. now form no longer sending successfully.

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -21,7 +21,7 @@ import {
   Select,
 } from "@chakra-ui/react";
 import MoonriseLogo from './MoonriseLogo'
-import CountryList from './CountryList'
+// import CountryList from './CountryList'
 import { m } from 'framer-motion';
 
 const enquiries = [
@@ -39,13 +39,11 @@ const countries = [
 
 const schema = z.object({
   email: z.string().email(),
-  firstName: z.string().min(1, { message: "Please enter your first name" }),
-  lastName: z.string().min(1, { message: "Please enter your last name" }),
+  name: z.string().min(1, { message: "Please enter your full name" }),
   company: z.string(),
   jobTitle: z.string(),
   phoneNumber: z.string().regex(/^\d{7,}$/i, { message: "Phone number must contain at least 7 digits" }),
   country: z.string().min(1, "Please select a country"),
-  enquiries: z.string().min(1, "Please select an enquiry"),
   message: z.string().min(1, "Please enter a message"),
 });
 
@@ -65,14 +63,17 @@ const ReactHookForm = () => {
     resolver: zodResolver(schema),
    });
    
-  const [state, setState] = useState(initState);
+  const [state, setState] = useState({ isLoading: false, error: "" });
   const [touched, setTouched] = useState({});
   const toast = useToast();
 
   const { isLoading, error } = state;
 
   const onSubmit: SubmitHandler<FormFields> = async (data) => {
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    setState((prev) => ({
+      ...prev,
+      isLoading: true,
+    }));
     try {
       await sendContactForm(data); // Call the sendContactForm function with form data
       reset()
@@ -84,15 +85,18 @@ const ReactHookForm = () => {
         duration: 6000,
         position: "top",
       });
-      // console.log('Form submitted successfully');
+      setState({ ...state, isLoading: false, error: "" });
+
     } catch (error) {
       console.error('Error submitting form:', error);
-      setState((prev) => ({
-        ...prev,
-        isLoading: false,
-        error: error.message,
-      
-      }))
+      setState({ ...state, isLoading: false, error: "Failed to send email. Please try again later." });
+      toast({
+        title: "Submission failed",
+        description: "There was an issue submitting the form. Please try again.",
+        status: "error",
+        duration: 6000,
+        position: "top",
+      });
     }
     console.log(data);
 
@@ -116,22 +120,13 @@ const ReactHookForm = () => {
       <form className='tutorial gap-2 w-full sm:w-3/5' onSubmit={handleSubmit(onSubmit)}>
         <div className='flex flex-col sm:flex-row gap-2'>  
           <div className='flex flex-col flex-1'>
-            <label htmlFor='firstName' className='custom-text pb-2'>First Name</label>
-            <Input {...register("firstName")} 
+            <label htmlFor='name' className='custom-text pb-2'>Full Name</label>
+            <Input {...register("name")} 
               type="text" 
-              id="firstName"
-              className={`p-3 rounded-md flex-1 bg-transparent border border-green-950 custom-text ${errors.firstName ? 'error-border' : ''}`}
+              id="name"
+              className={`p-3 rounded-md flex-1 bg-transparent border border-green-950 custom-text ${errors.name ? 'error-border' : ''}`}
             />
-            {errors.firstName && <span className='text-red-500'>{errors.firstName.message}</span>}
-          </div>
-          <div className='flex flex-col flex-1'>
-            <label htmlFor='lastName' className='custom-text pb-2'>Last Name</label>
-            <Input {...register("lastName")} 
-              type="text" 
-              id='lastName'
-              className={`p-3 rounded-md flex-1 bg-transparent border border-green-950 custom-text ${errors.lastName ? 'error-border' : ''}`}
-            />
-            {errors.lastName && <span className='text-red-500'>{errors.lastName.message}</span>}
+            {errors.name && <span className='text-red-500'>{errors.name.message}</span>}
           </div>
         </div>
 
@@ -160,15 +155,15 @@ const ReactHookForm = () => {
             <Input {...register("email")} 
               type="text" 
               id='email'
-              className={`p-3 rounded-md flex-1 bg-transparent border border-green-950 custom-text ${errors.firstName ? 'error-border' : ''}`}
+              className={`p-3 rounded-md flex-1 bg-transparent border border-green-950 custom-text ${errors.email ? 'error-border' : ''}`}
             />
             {errors.email && <span className='text-red-500'>{errors.email.message}</span>}
           </div>
           <div className='flex flex-col flex-1 '>
-            <label htmlFor='phone' className='custom-text pb-2'>Phone number</label>
+            <label htmlFor='phoneNumber' className='custom-text pb-2'>Phone number</label>
             <Input {...register("phoneNumber")} 
               type="text" 
-              id='phone'
+              id='phoneNumber'
               className={`p-3 rounded-md flex-1 bg-transparent border border-green-950 custom-text ${errors.phoneNumber ? 'error-border' : ''}`}  
             />
             {errors.phoneNumber && <span className='text-red-500'>{errors.phoneNumber.message}</span>}
@@ -192,7 +187,7 @@ const ReactHookForm = () => {
           </select>
         </div> */}
 
-        <FormControl borderColor={errors.country ? 'red.500' : 'green.950'}>
+        {/* <FormControl borderColor={errors.country ? 'red.500' : 'green.950'}>
           <label htmlFor='country' className='custom-text pb-2'>Country</label>
           <Select
             {...register("country")}
@@ -209,7 +204,18 @@ const ReactHookForm = () => {
               </option>
             ))}
           </Select>
-        </FormControl>
+        </FormControl> */}
+
+        <div className='flex flex-col sm:flex-row gap-2'>  
+          <div className='flex flex-col flex-1'>
+            <label htmlFor='country' className='custom-text pb-2'>Country</label>
+            <Input {...register("country")} 
+              type="text" 
+              id="country"
+              className={`p-3 rounded-md flex-1 bg-transparent border border-green-950 custom-text`}
+            />
+          </div>
+        </div>
 
         {/* <div className='flex flex-col flex-1'>
           <label htmlFor='enquiry' className='custom-text pb-2'>How Can We Help You</label>
@@ -229,27 +235,10 @@ const ReactHookForm = () => {
           </select>
         </div> */}
 
-        <FormControl borderColor={errors.enquiries ? 'red.500' : 'green.950'}>
-          <label htmlFor='enquiry' className='custom-text pb-2' style={{ marginBottom: '1rem' }}>How Can We Help You</label>
-          <Select
-            {...register("enquiries")}
-            id='enquiry'
-            className='p-3 rounded-md flex-1 bg-transparent text-slate-500' // Remove border styling here
-            defaultValue=""
-          >
-            <option value="" disabled>
-              Select one...
-            </option>
-            {enquiries.map((enquiry, index) => (
-              <option key={index} value={enquiry}>
-                {enquiry}
-              </option>
-            ))}
-          </Select>
-        </FormControl>
+
 
         <div className='flex flex-col flex-1'>
-          <label htmlFor='message' className='custom-text pb-2'>Message</label>
+          <label htmlFor='message' className='custom-text pb-2'>How can we help you?</label>
           <Textarea {...register("message")} 
             id='message'
             className={`p-3 rounded-md flex-1 bg-transparent border border-green-950 custom-text ${errors.message ? 'error-border' : ''}`}
