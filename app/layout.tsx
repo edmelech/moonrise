f2de39c2cf5936acbdb77fbc282af4caace33d7a
Ed Melech <mr.melech@gmail.com>
2024-06-10 22:16:51 +0100

    added favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,13 @@ const DynamicNavbar = dynamic(() => import('@/components/Navbar'), { ssr: false 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={`${inter.className} ${myFont.variable } `}>
       <Providers>
         <DynamicNavbar />
