24a2bef7e384c885f09799819ac6ad7ea3f5d71e
Ed Melech <mr.melech@gmail.com>
2024-08-06 18:50:29 +0100

    fix styling of image and text

--- components/ExpertiseContent.tsx
+++ components/ExpertiseContent.tsx
@@ -5,7 +5,7 @@ const ExpertiseContent: React.FC = () => {
   return (
     <div className="expertise-content m-8">
       <h2 className="text-xl font-bold mb-6">Expertise</h2>
-      <div className="content-wrapper flex">
+      <div className="content-wrapper flex mb-6">
         <div className="image-wrapper">
           <img src="/Water-Engineer-Clipboard.jpg" alt="Water Engineer Clipboard" className="rectangle-image" />
         </div>
@@ -30,33 +30,36 @@ const ExpertiseContent: React.FC = () => {
         </div>
       </div> 
       <div className="content-wrapper flex">
-      <ul className="list-none">
-        <li className="mb-6">
-          <div className="half-width-section-left">
-            <h3 className="mb-2 font-bold">Real Estate</h3>
-            <p className="mb-4">At moonrise. Our expert consultants specialise in strategically targeting top-tier candidates and partnering with clients across a broad portfolio of roles at real estate companies, from innovative start-ups to major multinationals from the commercial, residential and property investment, development and asset management sectors. We are passionate about people and property, with a team that understands the demands of real estate and go the extra mile to secure successful placements.</p>
-            <button className="learn-more">LEARN MORE</button>
+        <div className="text-and-image-wrapper flex">
+          <ul className="list-none">
+            <li className="mb-6">
+              <div className="half-width-section-left">
+                <h3 className="mb-2 font-bold">Real Estate</h3>
+                <p className="mb-4">At moonrise. Our expert consultants specialise in strategically targeting top-tier candidates and partnering with clients across a broad portfolio of roles at real estate companies, from innovative start-ups to major multinationals from the commercial, residential and property investment, development and asset management sectors. We are passionate about people and property, with a team that understands the demands of real estate and go the extra mile to secure successful placements.</p>
+                <button className="learn-more">LEARN MORE</button>
+              </div>
+            </li>
+            <li className="mb-6">
+              <div className="half-width-section-left">
+                <h3 className="mb-2 font-bold">Renewable Energy</h3>
+                <p className="mb-4">Our energy network consists of thousands of professionals with multi-disciplined capabilities as well as ample resources to serve global clients effectively and efficiently. Our resources and geographical base enable us to serve key local areas and partner efficiently with industry leaders in the energy field. Some of the disciplines covered by our network include Power Delivery Services, Battery Energy Storage Systems (BESS) Consulting, Hydrogen Services, Wind Engineering Services, Solar Engineering Services, Natural Gas Engineering Services, and Zero-Emission Vehicle Infrastructure.</p>
+                <button className="learn-more">LEARN MORE</button>
+              </div>
+            </li>
+          </ul>
+          <div className="image-wrapper">
+            <img src="/water-treatment-plant.jpg" alt="Water Treatment Plant" className="rectangle-image" />
           </div>
-        </li>
-        <li className="mb-6">
-          <div className="half-width-section-left">
-            <h3 className="mb-2 font-bold">Renewable Energy</h3>
-            <p className="mb-4">Our energy network consists of thousands of professionals with multi-disciplined capabilities as well as ample resources to serve global clients effectively and efficiently. Our resources and geographical base enable us to serve key local areas and partner efficiently with industry leaders in the energy field. Some of the disciplines covered by our network include Power Delivery Services, Battery Energy Storage Systems (BESS) Consulting, Hydrogen Services, Wind Engineering Services, Solar Engineering Services, Natural Gas Engineering Services, and Zero-Emission Vehicle Infrastructure.</p>
-            <button className="learn-more">LEARN MORE</button>
-          </div>
-        </li>
-        <div className="lower-half-width-section-right">
-        <li className="mb-6">
-          <div className="half-width-section-right">
-            <h3 className="mb-2 font-bold">Aviation / Aerospace</h3>
-            <p className="mb-4">We provide the expertise and ideas you need to confidently solve your aviation and aerospace talent management challenges. When it comes to the research, design, manufacture and maintenance of both aircraft and spacecraft, moonrise. has the people, strategy, and resources to provide the talent you require. We work with all kinds of organisations in the aviation and aerospace industry to facilitate growth and exploration. Our extensive networks and close regional knowledge allow us to access talent from around the globe.</p>
-            <button className="learn-more">LEARN MORE</button>
-          </div>
-        </li>
         </div>
-        
-      </ul>
       </div>
+      <div className="content-wrapper flex">
+        <div className="half-width-section-right mb-6">
+          <h3 className="mb-2 font-bold">Aviation / Aerospace</h3>
+          <p className="mb-4">We provide the expertise and ideas you need to confidently solve your aviation and aerospace talent management challenges. When it comes to the research, design, manufacture and maintenance of both aircraft and spacecraft, moonrise. has the people, strategy, and resources to provide the talent you require. We work with all kinds of organisations in the aviation and aerospace industry to facilitate growth and exploration. Our extensive networks and close regional knowledge allow us to access talent from around the globe.</p>
+          <button className="learn-more">LEARN MORE</button>
+        </div>
+      </div>
+      
     </div>
   );
 };
