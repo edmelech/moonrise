7adf4029760e8e2ea544d408ab423d68f6417117
Ed Melech <mr.melech@gmail.com>
2024-08-08 12:59:40 +0100

    made expertise page responsive

--- components/ExpertiseContent.tsx
+++ components/ExpertiseContent.tsx
@@ -5,13 +5,13 @@ const ExpertiseContent: React.FC = () => {
   return (
     <div className="expertise-content m-8">
       <h2 className="text-xl font-bold mb-6 ml-5">Expertise</h2>
-      <div className="content-wrapper flex mb-6">
-        <div className="image-wrapper">
-          <img src="/Water-Engineer-Clipboard.jpg" alt="Water Engineer Clipboard" className="rectangle-image" />
+      <div className="content-wrapper flex flex-col md:flex-row mb-6">
+        <div className="image-wrapper md:w-1/2 order-1 md:order-none">
+          <img src="/Water-Engineer-Clipboard.jpg" alt="Water Engineer Clipboard" className="rectangle-image w-full" />
         </div>
 
-        <div className="text-content">
-          <div className="half-width-section-right mb-6">
+        <div className="text-content md:w-1/2 order-2 md:order-none">
+          <div className="half-width-section-right mb-10">
             <h3 className="mb-2 font-bold">Engineering</h3>
             <p className="engineering-paragraph mb-4">At moonrise, we recognise that the engineering sector is growing at an astonishing rate. Our specialist
               consultants are experts in the engineering field and are committed to meeting the ever-growing demand
@@ -29,22 +29,22 @@ const ExpertiseContent: React.FC = () => {
           </div>
         </div>
       </div> 
-      <div className="content-wrapper flex">
-          <div className="text-content">
-              <div className="half-width-section-left">
+      <div className="content-wrapper flex flex-col md:flex-row">
+          <div className="text-content md:w-1/2 order-2 md:order-none">
+              <div className="half-width-section-left mb-10">
                 <h3 className="mb-2 font-bold">Real Estate</h3>
                 <p className="mb-4">At moonrise. Our expert consultants specialise in strategically targeting top-tier candidates and partnering with clients across a broad portfolio of roles at real estate companies, from innovative start-ups to major multinationals from the commercial, residential and property investment, development and asset management sectors. We are passionate about people and property, with a team that understands the demands of real estate and go the extra mile to secure successful placements.</p>
                 <button className="learn-more">LEARN MORE</button>
               </div>
 
-              <div className="half-width-section-left">
+              <div className="half-width-section-left mb-6">
                 <h3 className="mb-2 font-bold">Renewable Energy</h3>
                 <p className="mb-4">Our energy network consists of thousands of professionals with multi-disciplined capabilities as well as ample resources to serve global clients effectively and efficiently. Our resources and geographical base enable us to serve key local areas and partner efficiently with industry leaders in the energy field. Some of the disciplines covered by our network include Power Delivery Services, Battery Energy Storage Systems (BESS) Consulting, Hydrogen Services, Wind Engineering Services, Solar Engineering Services, Natural Gas Engineering Services, and Zero-Emission Vehicle Infrastructure.</p>
                 <button className="learn-more">LEARN MORE</button>
               </div>
           </div>
-          <div className="image-wrapper">
-            <img src="/water-treatment-plant.jpg" alt="Water Treatment Plant" className="rectangle-image" />
+          <div className="image-wrapper md:w-1/2 order-1 md:order-none">
+            <img src="/water-treatment-plant.jpg" alt="Water Treatment Plant" className="rectangle-image w-full" />
           </div>
       </div>
       <div className="text-content">
